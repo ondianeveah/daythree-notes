d1c50a7b34ed717c150b1548669296cb796a6265
Ondia Brown <Ondianeveah19@gmail.com>
2020-03-12 06:39:59 -0700

    this is cool

---
+++ src/main/java/testpack/Main.java
@@ -0,0 +1,14 @@
+package testpack;
+
+public class Main {
+    public static void main(String[] args) {
+        // Declaring a variable
+        byte x = 127;
+        System.out.println(x);
+
+        float myFloat = 3.12f;
+        System.out.println(myFloat);
+    }
+
+}
+
