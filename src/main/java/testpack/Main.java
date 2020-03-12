32d2b97696ef5aafd390a44c76111d1019af8c86
Ondia Brown <Ondianeveah19@gmail.com>
2020-03-12 06:58:26 -0700

    modified daythree notes

--- src/main/java/testpack/Main.java
+++ src/main/java/testpack/Main.java
@@ -3,11 +3,14 @@ package testpack;
 public class Main {
     public static void main(String[] args) {
         // Declaring a variable
-        byte x = 127;
-        System.out.println(x);
+ //       byte x = 127;
+  //      System.out.println(x);
 
-        float myFloat = 3.12f;
-        System.out.println(myFloat);
+  //      float myFloat = 3.12f;
+  //      System.out.println(myFloat);
+        for(int p = 1; p <= 5; p++){
+            System.out.println(p);
+        }
     }
 
 }
