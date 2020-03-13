2aa814ca5d4995f790b9ae3f5915b03b685cbf99
Ondia Brown <Ondianeveah19@gmail.com>
2020-03-13 05:59:10 -0700

    Thursday homework complete

---
+++ src/main/java/ThursdayHomework/ShapeTester.java
@@ -0,0 +1,9 @@
+package ThursdayHomework;
+
+public class ShapeTester {
+    public static void main(String[] args) {
+        Shape shape1 = new Shape ("Circles, triangles, and squares" );
+        shape1.rotate();
+        shape1.playsSound();
+    }
+}
